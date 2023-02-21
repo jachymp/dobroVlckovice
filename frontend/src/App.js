20f1d8a82a16cb2e1f2d750c95ba03d41bd4b28e
Jachym Pivonka <jachympivonka@gmail.com>
2023-02-21 08:12:12 +0100

    initial of slideshow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Slideshow from "./components/Slideshow";
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar/>
+        <Slideshow/>
     </BrowserRouter>
   );
 }
---
+++ frontend/src/components/Slideshow.js
@@ -0,0 +1,38 @@
+import './slideshow.css';
+import {useEffect, useState} from "react";
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28"];
+const delay = 2500;
+
+const Slideshow = () => {
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        setTimeout(
+            () =>
+                setIndex((prevIndex) =>
+                    prevIndex === colors.length -1 ? 0 : prevIndex + 1
+                ),
+            delay
+        );
+
+        return () => {};
+        }, [index]);
+    return (
+        <div className="slideshow">
+            <div className="slideshowSlider" style={{transform: `translate3d(${-index * 100}%, 0, 0)`}}>
+                {colors.map((backgroundColor, index) => (
+                    <div className="slide" key={index} style={{backgroundColor}}></div>
+                ))}
+            </div>
+            <div className="slideshowDots">
+                {colors.map((_,idx) => (
+                    <div key={idx} className={`slideshowDot${index === idx ? " active" : ""}`}></div>
+                ))}
+
+            </div>
+        </div>
+    );
+}
+
+export default Slideshow;
